d32ab4b91a65847eb0146b26bc2d97aabe120723
nicojoaquin <nicolas.joaquin@greelow.com>
2022-04-18 16:12:32 -0300

    installs

--- dist/helpers/idValidation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.idValidation = void 0;
-const idValidation = (item, itemName) => {
-    if (!item)
-        throw `No se encuentra el item '${itemName}' con ese id`;
-}; //Valida que el id busque algun item, y si no tira un error
-exports.idValidation = idValidation;
